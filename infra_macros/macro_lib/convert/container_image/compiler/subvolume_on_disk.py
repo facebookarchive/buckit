2e1bf355e4526c964045115d53dc1701ecff1f79
Alexey Spiridonov <lesha@fb.com>
2018-01-09 15:41:22 -0800

    Buck output-able disk representation for subvolumes
    Summary:
    After building a btrfs subvolume, we cannot just hand it to Buck, since it cannot be treated as a regular directory, and (unless `buck-out/` is in a btrfs volume, which is not guaranteed) it cannot even be placed in `buck-out`.
    
    So instead, we will build a JSON object that points to the actual subvolume. We will get the first variant of this object from our image builder (`from_build_buck_plumbing`). We then do a bit of processing to avoid storing anything but repo-relative paths in the JSON, and output it for real (`to_json_file`).
    
    This is a stand-alone module for testability reasons, and also because both the compiler & the subvolume garbage-collector need it.
    
    Reviewed By: marcinpe
    
    Differential Revision: D6681952
    
    fbshipit-source-id: 01b7bc0c52f8d880b743d82506b3175e9dc76981

---
+++ infra_macros/macro_lib/convert/container_image/compiler/subvolume_on_disk.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+#
+# Copyright 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+'See the SubvolumeOnDisk docblock.'
+import json
+import logging
+import os
+import socket
+import subprocess
+
+from collections import namedtuple
+
+log = logging.Logger(__name__)
+
+# These constants can represent both JSON keys for
+# serialization/deserialization, and namedtuple keys.  Legend:
+#  (1) Field in the namedtuple SubvolumeOnDisk
+#  (2) Parsed from the dictionary received from the image build tool's
+#      option `--print-buck-plumbing`
+#  (3) Output into the on-disk dictionary format
+#  (4) Read from the on-disk dictionary format
+_BTRFS_UUID = 'btrfs_uuid'  # (1-4)
+_HOSTNAME = 'hostname'  # (1-4)
+_SUBVOLUMES_DIR = 'subvolumes_dir'  # (1)
+_SUBVOLUME_PATH = 'subvolume_path'  # (2) + SubvolumeOnDisk.subvolume_path()
+_SUBVOLUME_NAME = 'subvolume_name'  # (1, 3-4)
+_SUBVOLUME_VERSION = 'subvolume_version'  # (1, 3-4)
+_DANGER = 'DANGER'  # (3)
+
+
+def _warn_on_unknown_keys(d, known_keys):
+    unknown_keys = known_keys - set(d.keys())
+    if unknown_keys:
+        log.warning(f'Unknown keys {unknown_keys} in {d}')  # pragma: no cover
+
+
+def _btrfs_get_volume_props(subvolume_path):
+    SNAPSHOTS = 'Snapshot(s)'
+    props = {}
+    # It's unfair to assume that the OS encoding is UTF-8, but our JSON
+    # serialization kind of requires it, and Python3 makes it hyper-annoying
+    # to work with bytestrings, so **shrug**.
+    #
+    # If this turns out to be a problem for a practical use case, we can add
+    # `surrogateescape` all over the place, or even set
+    # `PYTHONIOENCODING=utf-8:surrogateescape` in the environment.
+    for l in subprocess.check_output([
+        'sudo', 'btrfs', 'subvolume', 'show', subvolume_path,
+    ]).decode('utf-8').split('\n')[1:]:  # Skip the header line
+        if SNAPSHOTS in props:
+            if l:  # Ignore the trailing empty line
+                SPACES = 32
+                assert l[:SPACES] == ' ' * SPACES, f'Not a snapshot line: {l}'
+                props[SNAPSHOTS].append(l[SPACES:])
+        else:
+            k, v = l.strip().split(':', 1)
+            k = k.rstrip(':')
+            v = v.strip()
+            if k == SNAPSHOTS:
+                assert v == '', f'Should have nothing after ":" in: {l}'
+                props[SNAPSHOTS] = []
+            else:
+                assert k not in props, f'{l} already had a value {props[k]}'
+                props[k] = v
+    return props
+
+
+class SubvolumeOnDisk(namedtuple('SubvolumeOnDisk', [
+    _BTRFS_UUID,
+    _HOSTNAME,
+    _SUBVOLUMES_DIR,
+    _SUBVOLUME_NAME,
+    _SUBVOLUME_VERSION,
+])):
+    '''
+    This class stores a disk path to a btrfs subvolume (built image layer),
+    together with some minimal metadata about the layer.  It knows how to:
+     - parse the JSON output of the image build tool, containing the btrfs
+       subvolume path and some btrfs metadata.
+     - serialize & deserialize this metadata to a similar JSON format that
+       can be safely used as as Buck output representing the subvolume.
+    '''
+
+    _KNOWN_KEYS = {
+        _BTRFS_UUID,
+        _HOSTNAME,
+        _SUBVOLUME_NAME,
+        _SUBVOLUME_VERSION,
+        _DANGER,
+    }
+
+    def subvolume_path(self):
+        return os.path.join(
+            self.subvolumes_dir,
+            f'{self.subvolume_name}:{self.subvolume_version}'
+        )
+
+    @classmethod
+    def from_build_buck_plumbing(
+        cls, plumbing_output, subvolumes_dir, subvolume_name, subvolume_version
+    ):
+        d = json.loads(plumbing_output.decode())
+        _warn_on_unknown_keys(d, {
+            _BTRFS_UUID,
+            _HOSTNAME,
+            _SUBVOLUME_PATH,
+        })
+        self = cls(**{
+            _BTRFS_UUID: d.pop(_BTRFS_UUID),
+            _HOSTNAME: d.pop(_HOSTNAME),
+            _SUBVOLUMES_DIR: subvolumes_dir,
+            _SUBVOLUME_NAME: subvolume_name,
+            _SUBVOLUME_VERSION: subvolume_version,
+        })
+        subvolume_path = os.path.normpath(d.pop(_SUBVOLUME_PATH))
+        expected_subvolume_path = os.path.normpath(self.subvolume_path())
+        if subvolume_path != expected_subvolume_path:
+            raise RuntimeError(
+                f'Layer build returned unexpected subvolume_path '
+                f'{subvolume_path} != {expected_subvolume_path} from {d}'
+            )
+        # NB: No `._validate_and_return()` here, since it is redundant with
+        # the self-test that `to_serializable_dict()` will perform.
+        # Validation would also slow down `subvolume_garbage_collector.py`.
+        return self
+
+    @classmethod
+    def from_serializable_dict(cls, d, subvolumes_dir):
+        return cls(**{
+            _BTRFS_UUID: d[_BTRFS_UUID],
+            _HOSTNAME: d[_HOSTNAME],
+            _SUBVOLUME_NAME: d[_SUBVOLUME_NAME],
+            _SUBVOLUME_VERSION: d[_SUBVOLUME_VERSION],
+            _SUBVOLUMES_DIR: subvolumes_dir,
+        })._validate_and_return()
+
+    def to_serializable_dict(self):
+        # `subvolumes_dir` is an absolute path to a known location inside
+        # the repo.  We must not serialize it inside a Buck outputs, since
+        # that will break if the repo is moved.  Instead, we always
+        # recompute the path relative to the current subvolumes directory.
+        d = {
+            _BTRFS_UUID: self.btrfs_uuid,
+            _HOSTNAME: self.hostname,
+            _SUBVOLUME_NAME: self.subvolume_name,
+            _SUBVOLUME_VERSION: self.subvolume_version,
+            _DANGER: 'Do NOT edit manually: this can break future builds, or '
+                'break refcounting, causing us to leak or prematurely destroy '
+                'subvolumes.',
+        }
+        # Self-test -- there should be no way for this assertion to fail
+        new_self = self.from_serializable_dict(d, self.subvolumes_dir)
+        assert self == new_self, \
+          f'Got {new_self} from {d}, when serializing {self}'
+        return d
+
+    @classmethod
+    def from_json_file(cls, infile, subvolumes_dir):
+        parsed_json = '<NO JSON PARSED>'
+        try:
+            parsed_json = json.load(infile)
+            return cls.from_serializable_dict(
+                parsed_json, subvolumes_dir
+            )._validate_and_return()
+        except json.JSONDecodeError as ex:
+            raise RuntimeError(
+                f'Parsing subvolume JSON from {infile}: {ex.doc}'
+            ) from ex
+        except Exception as ex:
+            raise RuntimeError(
+                f'Parsed subvolume JSON from {infile}: {parsed_json}'
+            ) from ex
+
+    def to_json_file(self, outfile):
+        outfile.write(json.dumps(self.to_serializable_dict()))
+
+    def _validate_and_return(self):
+        cur_host = socket.getfqdn()
+        if cur_host != self.hostname:
+            raise RuntimeError(
+                f'Subvolume {self} did not come from current host {cur_host}'
+            )
+        # This incidentally checks that the subvolume exists and is btrfs.
+        volume_props = _btrfs_get_volume_props(self.subvolume_path())
+        if volume_props['UUID'] != self.btrfs_uuid:
+            raise RuntimeError(
+                f'UUID in subvolume JSON {self} does not match that of the '
+                f'actual subvolume {volume_props}'
+            )
+        return self
---
+++ infra_macros/macro_lib/convert/container_image/compiler/tests/test_subvolume_on_disk.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+import io
+import json
+import os
+import subvolume_on_disk
+import unittest
+import unittest.mock
+
+_MY_HOST = 'my_host'
+
+
+class SubvolumeOnDiskTestCase(unittest.TestCase):
+
+    def _test_uuid(self, subvolume_path):
+        if self._mock_uuid_stack:
+            return self._mock_uuid_stack.pop()
+        return f'test_uuid_of:{subvolume_path}'
+
+    def setUp(self):
+        'Configure mocks shared by most of the tests.'
+        self._mock_uuid_stack = []
+
+        self.patch_btrfs_get_volume_props = unittest.mock.patch(
+            'subvolume_on_disk._btrfs_get_volume_props'
+        )
+        self.mock_btrfs_get_volume_props = \
+            self.patch_btrfs_get_volume_props.start()
+        self.mock_btrfs_get_volume_props.side_effect = lambda subvolume_path: {
+            # Since we key the uuid off the given argument, we don't have to
+            # explicitly validate the given path for each mock call.
+            'UUID': self._test_uuid(subvolume_path),
+        }
+        self.addCleanup(self.patch_btrfs_get_volume_props.stop)
+
+        self.patch_getfqdn = unittest.mock.patch('socket.getfqdn')
+        self.mock_getfqdn = self.patch_getfqdn.start()
+        self.mock_getfqdn.side_effect = lambda: _MY_HOST
+        self.addCleanup(self.patch_getfqdn.stop)
+
+    def _check(self, actual_subvol, expected_path, expected_subvol):
+        self.assertEqual(expected_path, actual_subvol.subvolume_path())
+        self.assertEqual(expected_subvol, actual_subvol)
+
+        # Automatically tests "normal case" serialization & deserialization
+        fake_file = io.StringIO()
+
+        # We'll consume UUIDs twice: once for the `to` build-in self-test,
+        # once for the `from` validation. So add two of the right UUID.
+        stack_size = len(self._mock_uuid_stack)
+        self._mock_uuid_stack.extend([actual_subvol.btrfs_uuid] * 2)
+
+        actual_subvol.to_json_file(fake_file)
+        fake_file.seek(0)
+
+        self._mock_uuid_stack.append(actual_subvol.btrfs_uuid)
+        self.assertEqual(
+            actual_subvol,
+            subvolume_on_disk.SubvolumeOnDisk.from_json_file(
+                fake_file, actual_subvol.subvolumes_dir
+            ),
+        )
+
+        self.assertEqual(stack_size, len(self._mock_uuid_stack))
+
+    def test_from_json_file_errors(self):
+        with self.assertRaisesRegex(RuntimeError, 'Parsing subvolume JSON'):
+            subvolume_on_disk.SubvolumeOnDisk.from_json_file(
+                io.StringIO('invalid json'), '/subvols'
+            )
+        with self.assertRaisesRegex(RuntimeError, 'Parsed subvolume JSON'):
+            subvolume_on_disk.SubvolumeOnDisk.from_json_file(
+                io.StringIO('5'), '/subvols'
+            )
+
+    def test_from_serializable_dict_and_validation(self):
+        # Note: Unlike test_from_build_buck_plumbing, this test uses a
+        # trailing / -- this gets us better coverage.
+        subvols = '/test_subvols/'
+        good_path = os.path.join(subvols, 'test_name:test_ver')
+        good_uuid = self._test_uuid(good_path)
+        good = {
+            subvolume_on_disk._BTRFS_UUID: good_uuid,
+            subvolume_on_disk._HOSTNAME: _MY_HOST,
+            subvolume_on_disk._SUBVOLUME_NAME: 'test_name',
+            subvolume_on_disk._SUBVOLUME_VERSION: 'test_ver',
+        }
+
+        bad_host = good.copy()
+        bad_host[subvolume_on_disk._HOSTNAME] = f'NOT_{_MY_HOST}'
+        with self.assertRaisesRegex(
+            RuntimeError, 'did not come from current host'
+        ):
+            subvolume_on_disk.SubvolumeOnDisk.from_serializable_dict(
+                bad_host, subvols
+            )
+
+        bad_uuid = good.copy()
+        bad_uuid[subvolume_on_disk._BTRFS_UUID] = 'BAD_UUID'
+        with self.assertRaisesRegex(
+            RuntimeError, 'UUID in subvolume JSON .* does not match'
+        ):
+            subvolume_on_disk.SubvolumeOnDisk.from_serializable_dict(
+                bad_uuid, subvols
+            )
+
+        # Parsing the `good` dict does not throw, and gets the right result.
+        good_subvol = subvolume_on_disk.SubvolumeOnDisk.from_serializable_dict(
+            good, subvols
+        )
+        self._check(
+            good_subvol,
+            good_path,
+            subvolume_on_disk.SubvolumeOnDisk(
+                btrfs_uuid=good_uuid,
+                hostname=_MY_HOST,
+                subvolume_name='test_name',
+                subvolume_version='test_ver',
+                subvolumes_dir=subvols,
+            ),
+        )
+
+    def test_from_build_buck_plumbing(self):
+        # Note: Unlike test_from_serializable_dict_and_validation, this test
+        # does NOT use a trailing / -- this gets us better coverage.
+        subvols = '/test_subvols'
+        subvol_path = '/test_subvols/test_name:test_ver'
+        plumbing_output = json.dumps({
+            'btrfs_uuid': 'test_uuid',
+            'hostname': _MY_HOST,
+            'subvolume_path': subvol_path,
+        }).encode()
+
+        good_args = {
+            'plumbing_output': plumbing_output,
+            'subvolumes_dir': subvols,
+            'subvolume_name': 'test_name',
+            'subvolume_version': 'test_ver',
+        }
+        subvol = subvolume_on_disk.SubvolumeOnDisk.from_build_buck_plumbing(
+            **good_args
+        )
+        self._check(
+            subvol,
+            subvol_path,
+            subvolume_on_disk.SubvolumeOnDisk(
+                btrfs_uuid='test_uuid',
+                hostname=_MY_HOST,
+                subvolume_name='test_name',
+                subvolume_version='test_ver',
+                subvolumes_dir=subvols,
+            ),
+        )
+
+        bad_subvols = good_args.copy()
+        bad_subvols['subvolumes_dir'] = '/bad_subvols/'
+        bad_name = good_args.copy()
+        bad_name['subvolume_name'] = 'bad_name'
+        bad_ver = good_args.copy()
+        bad_ver['subvolume_version'] = 'bad_ver'
+
+        for bad_args in [bad_subvols, bad_name, bad_ver]:
+            with self.assertRaisesRegex(
+                RuntimeError, 'unexpected subvolume_path'
+            ):
+                subvolume_on_disk.SubvolumeOnDisk.from_build_buck_plumbing(
+                    **bad_args
+                )
+
+
+class BtrfsVolumePropsTestCase(unittest.TestCase):
+    'Separate from SubvolumeOnDiskTestCase because to avoid its mocks.'
+
+    @unittest.mock.patch('subprocess.check_output')
+    def test_btrfs_get_volume_props(self, check_output):
+        parent = '/subvols/dir/parent'
+        check_output.return_value = b'''\
+dir/parent
+        Name:                   parent
+        UUID:                   f96b940f-10d3-fc4e-8b2d-9362af0ee8df
+        Parent UUID:            -
+        Received UUID:          -
+        Creation time:          2017-12-29 21:55:54 -0800
+        Subvolume ID:           277
+        Generation:             123
+        Gen at creation:        103
+        Parent ID:              5
+        Top level ID:           5
+        Flags:                  readonly
+        Snapshot(s):
+                                dir/foo
+                                dir/bar
+'''
+        self.assertEquals(
+            subvolume_on_disk._btrfs_get_volume_props(parent),
+            {
+                'Name': 'parent',
+                'UUID': 'f96b940f-10d3-fc4e-8b2d-9362af0ee8df',
+                'Parent UUID': '-',
+                'Received UUID': '-',
+                'Creation time': '2017-12-29 21:55:54 -0800',
+                'Subvolume ID': '277',
+                'Generation': '123',
+                'Gen at creation': '103',
+                'Parent ID': '5',
+                'Top level ID': '5',
+                'Flags': 'readonly',
+                'Snapshot(s)': ['dir/foo', 'dir/bar'],
+            }
+        )
+        check_output.assert_called_once_with(
+            ['sudo', 'btrfs', 'subvolume', 'show', parent]
+        )
+
+        # Unlike the parent, this has no snapshots, so the format differs.
+        child = '/subvols/dir/child'
+        check_output.reset_mock()
+        check_output.return_value = b'''\
+dir/child
+        Name:                   child
+        UUID:                   a1a3eb3e-eb89-7743-8335-9cd5219248e7
+        Parent UUID:            f96b940f-10d3-fc4e-8b2d-9362af0ee8df
+        Received UUID:          -
+        Creation time:          2017-12-29 21:56:32 -0800
+        Subvolume ID:           278
+        Generation:             121
+        Gen at creation:        107
+        Parent ID:              5
+        Top level ID:           5
+        Flags:                  -
+        Snapshot(s):
+'''
+        self.assertEquals(
+            subvolume_on_disk._btrfs_get_volume_props(child),
+            {
+                'Name': 'child',
+                'UUID': 'a1a3eb3e-eb89-7743-8335-9cd5219248e7',
+                'Parent UUID': 'f96b940f-10d3-fc4e-8b2d-9362af0ee8df',
+                'Received UUID': '-',
+                'Creation time': '2017-12-29 21:56:32 -0800',
+                'Subvolume ID': '278',
+                'Generation': '121',
+                'Gen at creation': '107',
+                'Parent ID': '5',
+                'Top level ID': '5',
+                'Flags': '-',
+                'Snapshot(s)': [],
+            }
+        )
+        check_output.assert_called_once_with(
+            ['sudo', 'btrfs', 'subvolume', 'show', child]
+        )
