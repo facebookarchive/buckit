387b7e189ba5de99c02ee9c450dfeb7c04c8965b
Andrew Gallagher <andrewjcg@fb.com>
2018-08-03 16:04:34 -0700

    buck macros: make sure changes to thrift2 cause generated cpp2 code to rebuild
    Summary:
    As per comments, this restores a pre-D5433016 workaround to make thrift2 changes
    invalidate cache keys for generated cpp2 code.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D9162543
    
    fbshipit-source-id: 33071523104ceafa379698c17a896c9f1d899bb3

--- infra_macros/macro_lib/convert/thrift_library.py
+++ infra_macros/macro_lib/convert/thrift_library.py
@@ -204,10 +204,21 @@ class ThriftLangConverter(base.Converter):
         cmd.append('-o')
         cmd.append('"$OUT"')
         additional_compiler = self.get_additional_compiler()
+        # TODO(T17324385): Work around an issue where dep chains of binaries
+        # (e.g. one binary which delegates to another when it runs), aren't
+        # added to the rule key when run in `genrule`s.  So, we need to
+        # explicitly add these deps to the `genrule` using `$(exe ...)`.
+        # However, since the implemenetation of `--python-compiler` in thrift
+        # requires a single file path, and since `$(exe ...)` actually expands
+        # to a list of args, we ues `$(query_outputs ...)` here and just append
+        # `$(exe ...)` to the end of the command below, in a comment.
         if additional_compiler:
             cmd.append('--python-compiler')
             cmd.append('$(query_outputs "{}")'.format(additional_compiler))
         cmd.append('"$SRCS"')
+        # TODO(T17324385): Followup mentioned in above comment.
+        if additional_compiler:
+            cmd.append('# $(exe {})'.format(additional_compiler))
         return ' '.join(cmd)
 
     def get_generated_sources(
