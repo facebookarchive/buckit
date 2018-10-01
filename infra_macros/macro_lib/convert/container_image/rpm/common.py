230f019aacc2abcd0a8021ea2967a5f2453bc1a5
Alexey Spiridonov <lesha@fb.com>
2018-10-01 15:46:31 -0700

    Add `check_popen_returncode` to "common" library
    Summary: This is used by FB-specific storage engines.
    
    Reviewed By: marcinpe
    
    Differential Revision: D10123314
    
    fbshipit-source-id: 7d425eb768d058bf43fee4b5f4f62b11e601d692

---
+++ infra_macros/macro_lib/convert/container_image/rpm/common.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+def check_popen_returncode(proc: subprocess.Popen):
+    if proc.returncode != 0:  # pragma: no cover
+        # Providing a meaningful coverage test for this is annoying, so I just
+        # tested manually:
+        #   >>> import subprocess
+        #   >>> raise subprocess.CalledProcessError(returncode=5, cmd=['a'])
+        #   Traceback (most recent call last):
+        #     File "<stdin>", line 1, in <module>
+        #   subprocess.CalledProcessError: Command '['a']' returned non-zero
+        #   exit status 5.
+        raise subprocess.CalledProcessError(
+            returncode=proc.returncode, cmd=proc.args,
+        )
