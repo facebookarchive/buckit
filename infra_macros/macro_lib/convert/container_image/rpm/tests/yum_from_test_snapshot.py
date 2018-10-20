c48ec53c75c84ae26aece0fc6115f3b664e28e95
Alexey Spiridonov <lesha@fb.com>
2018-10-19 20:59:36 -0700

    Simplify `yum-from-test-snapshot` now that our `python_library` takes `resources`
    Summary: D10470396's description has more detail, but in a nutshell, this makes `yum-from-test-snapshot` access snapshots in exactly the same way we would for prod snapshots.
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D10470463
    
    fbshipit-source-id: 75f796caae8642402841e38d38721812c92b0ace

--- infra_macros/macro_lib/convert/container_image/rpm/tests/yum_from_test_snapshot.py
+++ infra_macros/macro_lib/convert/container_image/rpm/tests/yum_from_test_snapshot.py
@@ -5,31 +5,24 @@ are built by `tests/build_repos.py`. Used in the image compiler unit tests.
 '''
 import json
 import os
-import tarfile
-import tempfile
 
 from ..common import init_logging, Path
 from ..yum_from_snapshot import add_common_yum_args, yum_from_snapshot
 
 
 def yum_from_test_snapshot(install_root: 'AnyStr', yum_args: 'List[AnyStr]'):
-    with tempfile.TemporaryDirectory() as snapshot_dir:
-        with tarfile.open(
-            # This works in @mode/opt since the .tgz is baked into the XAR
-            Path(os.path.dirname(__file__)) / 'snapshot.tgz'
-        ) as tar:
-            tar.extractall(snapshot_dir)
-        snapshot_dir = Path(snapshot_dir) / 'snapshot'
-        yum_from_snapshot(
-            storage_cfg=json.dumps({
-                'key': 'test',
-                'kind': 'filesystem',
-                'base_dir': (snapshot_dir / 'storage').decode(),
-            }),
-            snapshot_dir=snapshot_dir / 'repos',
-            install_root=Path(install_root),
-            yum_args=yum_args,
-        )
+    # This works in @mode/opt since the snapshot is baked into the XAR
+    snapshot_dir = Path(os.path.dirname(__file__)) / 'snapshot'
+    yum_from_snapshot(
+        storage_cfg=json.dumps({
+            'key': 'test',
+            'kind': 'filesystem',
+            'base_dir': (snapshot_dir / 'storage').decode(),
+        }),
+        snapshot_dir=snapshot_dir / 'repos',
+        install_root=Path(install_root),
+        yum_args=yum_args,
+    )
 
 
 # CLI tested indirectly via the image compiler's test image targets.
