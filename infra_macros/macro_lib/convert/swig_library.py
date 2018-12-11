afc2870e0fa7e78ff49f5a5a19c1eb9ac392aa41
Taras Tsugrii <ttsugrii@fb.com>
2018-12-10 21:51:10 -0800

    Use fb_native rules directly in JavaSwigConverter.
    Reviewed By: andrewjcg
    
    Differential Revision: D13413490
    
    fbshipit-source-id: 6926e7f8e3c06b74c37c79721d3ca34ac237b275

--- infra_macros/macro_lib/convert/swig_library.py
+++ infra_macros/macro_lib/convert/swig_library.py
@@ -42,6 +42,11 @@ load("@fbcode_macros//build_defs/lib:python_typing.bzl",
 load("@fbcode_macros//build_defs/lib:merge_tree.bzl", "merge_tree")
 load("@fbcode_macros//build_defs/lib:copy_rule.bzl", "copy_rule")
 load("@fbcode_macros//build_defs/lib:src_and_dep_helpers.bzl", "src_and_dep_helpers")
+load(
+    "@fbsource//tools/build_defs:fb_native_wrapper.bzl",
+    "fb_native",
+)
+load("@fbcode_macros//build_defs/lib:visibility.bzl", "get_visibility")
 
 
 FLAGS = [
@@ -142,64 +147,62 @@ class JavaSwigConverter(SwigLangConverter):
             java_library_name=None,
             visibility=None,
             **kwargs):
-
-        rules = []
-
         # Build the C/C++ Java extension from the generated C/C++ sources.
-        attrs = collections.OrderedDict()
-        attrs['name'] = name + '-ext'
-        if visibility is not None:
-            attrs['visibility'] = visibility
-        attrs['srcs'] = [src]
-        # Swig-generated code breaks strict-aliasing with gcc
-        # (http://www.swig.org/Doc3.0/SWIGDocumentation.html#Java_compiling_dynamic).
-        attrs['compiler_flags'] = ['-fno-strict-aliasing']
-        attrs['soname'] = (
-            'lib{}.so'.format(
-                module if java_library_name is None else java_library_name))
-        attrs['link_style'] = kwargs.get('java_link_style')
-        attrs['deps'], attrs['platform_deps'] = (
-            src_and_dep_helpers.format_all_deps(
-                cpp_deps + [target_utils.RootRuleTarget('common/java/jvm', 'jvm')]))
-        # When using e.g. %feature("director") in Something.i, SWIG includes
-        # "Something.h" in the source code of the C/C++ Java extension.
-        attrs['headers'] = [hdr]
-        attrs['header_namespace'] = ''
+        ext_name = name + '-ext'
         # Setup platform default for compilation DB, and direct building.
         buck_platform = platform_utils.get_buck_platform_for_base_path(base_path)
-        attrs['default_platform'] = buck_platform
-        attrs['defaults'] = {'platform': buck_platform}
-        rules.append(Rule('cxx_library', attrs))
+        ext_deps, ext_platform_deps = (
+            src_and_dep_helpers.format_all_deps(
+                cpp_deps + [target_utils.RootRuleTarget('common/java/jvm', 'jvm')]))
+
+        fb_native.cxx_library(
+            name=ext_name,
+            visibility=get_visibility(visibility, ext_name),
+            srcs=[src],
+            # Swig-generated code breaks strict-aliasing with gcc
+            # (http://www.swig.org/Doc3.0/SWIGDocumentation.html#Java_compiling_dynamic).
+            compiler_flags=['-fno-strict-aliasing'],
+            soname=(
+                'lib{}.so'.format(
+                    module if java_library_name is None else java_library_name)),
+            link_style=kwargs.get('java_link_style'),
+            deps=ext_deps,
+            platform_deps=ext_platform_deps,
+            # When using e.g. %feature("director") in Something.i, SWIG includes
+            # "Something.h" in the source code of the C/C++ Java extension.
+            headers=[hdr],
+            header_namespace='',
+            default_platform=buck_platform,
+            defaults={'platform': buck_platform},
+        )
 
         # Pack all generated source directories into a source zip, which we'll
         # feed into the Java library rule.
         src_zip_name = name + '.src.zip'
-        attrs = collections.OrderedDict()
-        attrs['name'] = src_zip_name
-        if visibility is not None:
-            attrs['visibility'] = visibility
-        # Java rules are C/C++ platform agnostic, so we're forced to choose a
-        # fixed platform at parse-time (which means Java binaries will only
-        # ever build against one platform at a time).
-        attrs['srcs'] = (
-            ['{}#{}'.format(s, platform_utils.get_buck_platform_for_base_path(base_path))
-             for s in gen_srcs.values()])
-        attrs['out'] = src_zip_name
-        rules.append(Rule('zip_file', attrs))
+        fb_native.zip_file(
+            name=src_zip_name,
+            visibility=get_visibility(visibility, src_zip_name),
+            # Java rules are C/C++ platform agnostic, so we're forced to choose a
+            # fixed platform at parse-time (which means Java binaries will only
+            # ever build against one platform at a time).
+            srcs=(
+                ['{}#{}'.format(s, platform_utils.get_buck_platform_for_base_path(base_path))
+                 for s in gen_srcs.values()]),
+            out=src_zip_name,
+        )
 
         # Generate the wrapping Java library.
-        attrs = collections.OrderedDict()
-        attrs['name'] = name
-        if visibility is not None:
-            attrs['visibility'] = visibility
-        attrs['srcs'] = [':' + src_zip_name]
         out_deps = []
         out_deps.extend(deps)
         out_deps.append(':' + name + '-ext')
-        attrs['deps'] = out_deps
-        rules.append(Rule('java_library', attrs))
+        fb_native.java_library(
+            name=name,
+            visibility=get_visibility(visibility, name),
+            srcs=[':' + src_zip_name],
+            deps=out_deps,
+        )
 
-        return rules
+        return []
 
 
 class PythonSwigConverter(SwigLangConverter):
