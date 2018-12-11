ef086b9085412152027b8be7a81c64b74a9e0ab1
Taras Tsugrii <ttsugrii@fb.com>
2018-12-10 18:44:47 -0800

    Migrate cpp_jvm_library to Skylark.
    Reviewed By: andrewjcg
    
    Differential Revision: D13400318
    
    fbshipit-source-id: e704a63e048f89d46fe03434f415be2e394490d4

---
+++ infra_macros/fbcode_macros/tests/cpp_jvm_library_test.py
@@ -0,0 +1,33 @@
+# Copyright 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tests.utils
+from tests.utils import dedent
+
+
+class CppJvmLibraryTest(tests.utils.TestCase):
+    includes = [("@fbcode_macros//build_defs:cpp_jvm_library.bzl", "cpp_jvm_library")]
+
+    @tests.utils.with_project()
+    def test_cpp_jvm_library_parses(self, root):
+        root.addFile(
+            "BUCK",
+            dedent(
+                """
+            load("@fbcode_macros//build_defs:cpp_jvm_library.bzl", "cpp_jvm_library")
+            cpp_jvm_library(
+                name = "jvm",
+                major_version = 8,
+            )
+
+            """
+            ),
+        )
+
+        self.assertSuccess(root.runAudit(["BUCK"]))
