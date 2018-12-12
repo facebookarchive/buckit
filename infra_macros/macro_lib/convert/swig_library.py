95f0c940cab9a07652cd9142c485ae7bd1272573
Taras Tsugrii <ttsugrii@fb.com>
2018-12-11 19:47:13 -0800

    Extract swig go_converter to Skylark.
    Summary:
    This reimplements the logic of go conversion from swig_library
    in Skylark.
    
    Reviewed By: andrewjcg
    
    Differential Revision: D13425693
    
    fbshipit-source-id: e94413d9f77d62e038d2d532bd9c03ef71bce563

--- infra_macros/macro_lib/convert/swig_library.py
+++ infra_macros/macro_lib/convert/swig_library.py
@@ -41,6 +41,7 @@ load(
 )
 load("@fbcode_macros//build_defs/lib:common_paths.bzl", "common_paths")
 load("@fbcode_macros//build_defs/lib:visibility.bzl", "get_visibility")
+load("@fbcode_macros//build_defs/lib/swig:go_converter.bzl", "go_converter")
 load("@bazel_skylib//lib:shell.bzl", "shell")
 load("@bazel_skylib//lib:paths.bzl", "paths")
 
@@ -288,71 +289,6 @@ class PythonSwigConverter(SwigLangConverter):
         return []
 
 
-class GoSwigConverter(SwigLangConverter):
-    """
-    Specializer to support generating Go libraries from swig sources.
-    """
-
-    def __init__(self, context, *args, **kwargs):
-        super(GoSwigConverter, self).__init__(context, *args, **kwargs)
-
-    def get_lang(self):
-        return 'go'
-
-    def get_lang_opt(self):
-        return '-go'
-
-    def get_lang_flags(self, go_package_name=None, **kwargs):
-        return [
-            '-cgo',
-            '-intgosize', '64',  # should fit most of the cases
-            '-module', go_package_name
-        ]
-
-    def get_generated_sources(self, module):
-        src = module + '.go'
-        return {src: src}
-
-    def get_language_rule(
-            self,
-            base_path,
-            name,
-            module,
-            hdr,
-            src,
-            gen_srcs,
-            cpp_deps,
-            deps,
-            go_package_name=None,
-            visibility=None,
-            **kwargs):
-        # create wrapper cxx_library rule that includes generated .cc files
-        for dep in cpp_deps:
-            deps.extend(src_and_dep_helpers.format_deps([target_utils.RuleTarget(
-                name="{}-ext".format(dep.name),
-                base_path=dep.base_path,
-                repo=dep.repo)]))
-            fb_native.cxx_library(
-                name="{}-ext".format(dep.name),
-                deps=src_and_dep_helpers.format_deps([dep]),
-                srcs=[src],
-            )
-
-        # generate the cgo_library
-        fb_native.cgo_library(
-            name=name,
-            package_name=go_package_name,
-            visibility=get_visibility(visibility, name),
-            # platform is required for cxx_genrule (copied from java)
-            srcs=(
-                ['{}#{}'.format(s, platform_utils.get_buck_platform_for_base_path(base_path))
-                 for s in gen_srcs.values()]),
-            deps=deps,
-        )
-
-        return []
-
-
 class SwigLibraryConverter(base.Converter):
 
     def __init__(self, *args, **kwargs):
@@ -361,7 +297,7 @@ class SwigLibraryConverter(base.Converter):
         # Setup the macro converters.
         converters = [
             JavaSwigConverter(*args, **kwargs),
-            GoSwigConverter(*args, **kwargs),
+            go_converter,
             PythonSwigConverter(*args, **kwargs),
         ]
         self._converters = {c.get_lang(): c for c in converters}
