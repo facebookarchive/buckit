33e61fef56190d14dbfe61b065423974ba4c156c
Taras Tsugrii <ttsugrii@fb.com>
2018-12-11 19:47:13 -0800

    Extract swig java_converter to Skylark.
    Summary:
    This change replicates the logic of java converter in
    swig_library.py in Skylark.
    
    Reviewed By: andrewjcg
    
    Differential Revision: D13425724
    
    fbshipit-source-id: 6fe88f0f430e82d84bb8d766845b279013bc3b46

--- infra_macros/macro_lib/convert/swig_library.py
+++ infra_macros/macro_lib/convert/swig_library.py
@@ -42,6 +42,7 @@ load(
 load("@fbcode_macros//build_defs/lib:common_paths.bzl", "common_paths")
 load("@fbcode_macros//build_defs/lib:visibility.bzl", "get_visibility")
 load("@fbcode_macros//build_defs/lib/swig:go_converter.bzl", "go_converter")
+load("@fbcode_macros//build_defs/lib/swig:java_converter.bzl", "java_converter")
 load("@bazel_skylib//lib:shell.bzl", "shell")
 load("@bazel_skylib//lib:paths.bzl", "paths")
 
@@ -105,101 +106,6 @@ class SwigLangConverter(base.Converter):
         """
 
         raise NotImplementedError()
-
-
-class JavaSwigConverter(SwigLangConverter):
-    """
-    Specializer to support generating Java libraries from swig sources.
-    """
-
-    def get_lang(self):
-        return 'java'
-
-    def get_lang_opt(self):
-        return '-java'
-
-    def get_lang_flags(self, java_package=None, **kwargs):
-        flags = []
-
-        # Forward the user-provided `java_package` parameter.
-        if java_package is not None:
-            flags.append('-package')
-            flags.append(java_package)
-
-        return flags
-
-    def get_generated_sources(self, module):
-        return {'': '.'}
-
-    def get_language_rule(
-            self,
-            base_path,
-            name,
-            module,
-            hdr,
-            src,
-            gen_srcs,
-            cpp_deps,
-            deps,
-            java_library_name=None,
-            visibility=None,
-            **kwargs):
-        # Build the C/C++ Java extension from the generated C/C++ sources.
-        ext_name = name + '-ext'
-        # Setup platform default for compilation DB, and direct building.
-        buck_platform = platform_utils.get_buck_platform_for_base_path(base_path)
-        ext_deps, ext_platform_deps = (
-            src_and_dep_helpers.format_all_deps(
-                cpp_deps + [target_utils.RootRuleTarget('common/java/jvm', 'jvm')]))
-
-        fb_native.cxx_library(
-            name=ext_name,
-            visibility=get_visibility(visibility, ext_name),
-            srcs=[src],
-            # Swig-generated code breaks strict-aliasing with gcc
-            # (http://www.swig.org/Doc3.0/SWIGDocumentation.html#Java_compiling_dynamic).
-            compiler_flags=['-fno-strict-aliasing'],
-            soname=(
-                'lib{}.so'.format(
-                    module if java_library_name is None else java_library_name)),
-            link_style=kwargs.get('java_link_style'),
-            deps=ext_deps,
-            platform_deps=ext_platform_deps,
-            # When using e.g. %feature("director") in Something.i, SWIG includes
-            # "Something.h" in the source code of the C/C++ Java extension.
-            headers=[hdr],
-            header_namespace='',
-            default_platform=buck_platform,
-            defaults={'platform': buck_platform},
-        )
-
-        # Pack all generated source directories into a source zip, which we'll
-        # feed into the Java library rule.
-        src_zip_name = name + '.src.zip'
-        fb_native.zip_file(
-            name=src_zip_name,
-            visibility=get_visibility(visibility, src_zip_name),
-            # Java rules are C/C++ platform agnostic, so we're forced to choose a
-            # fixed platform at parse-time (which means Java binaries will only
-            # ever build against one platform at a time).
-            srcs=(
-                ['{}#{}'.format(s, platform_utils.get_buck_platform_for_base_path(base_path))
-                 for s in gen_srcs.values()]),
-            out=src_zip_name,
-        )
-
-        # Generate the wrapping Java library.
-        out_deps = []
-        out_deps.extend(deps)
-        out_deps.append(':' + name + '-ext')
-        fb_native.java_library(
-            name=name,
-            visibility=get_visibility(visibility, name),
-            srcs=[':' + src_zip_name],
-            deps=out_deps,
-        )
-
-        return []
 
 
 class PythonSwigConverter(SwigLangConverter):
@@ -296,7 +202,7 @@ class SwigLibraryConverter(base.Converter):
 
         # Setup the macro converters.
         converters = [
-            JavaSwigConverter(*args, **kwargs),
+            java_converter,
             go_converter,
             PythonSwigConverter(*args, **kwargs),
         ]
