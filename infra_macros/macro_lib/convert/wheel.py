ea506c37561b49e85f3906f2d440c5d14c759b14
Taras Tsugrii <ttsugrii@fb.com>
2018-12-13 23:15:02 -0800

    Migrate python_wheel to Skylark.
    Reviewed By: andrewjcg
    
    Differential Revision: D13463224
    
    fbshipit-source-id: 3895546b8b245faca2c5cf3a91d0d25f036a1e90

--- infra_macros/macro_lib/convert/wheel.py
+++ infra_macros/macro_lib/convert/wheel.py
@@ -38,38 +38,13 @@ load("@fbcode_macros//build_defs/lib:python_typing.bzl",
      "get_typing_config_target")
 
 load("@fbcode_macros//build_defs:platform_utils.bzl", "platform_utils")
+load("@fbcode_macros//build_defs:python_wheel.bzl", "python_wheel")
 load("@fbcode_macros//build_defs/lib:src_and_dep_helpers.bzl", "src_and_dep_helpers")
 load("@fbcode_macros//build_defs/lib:python_typing.bzl", "gen_typing_config")
 load("@fbcode_macros//build_defs/lib:visibility.bzl", "get_visibility")
 load("@fbsource//tools/build_defs:fb_native_wrapper.bzl", "fb_native")
 load("@bazel_skylib//lib:paths.bzl", "paths")
 load("@bazel_skylib//lib:shell.bzl", "shell")
-
-
-def get_url_basename(url):
-    """ Urls will have an #md5 etag remove it and return the wheel name"""
-    return paths.basename(url).rsplit('#md5=')[0]
-
-
-def remote_wheel(url, out, sha1, visibility):
-    remote_file_name = out + '-remote'
-    fb_native.remote_file(
-        name=remote_file_name,
-        visibility=get_visibility(visibility, remote_file_name),
-        out=out,
-        url=url,
-        sha1=sha1,
-    )
-    return ":" + remote_file_name
-
-
-def prebuilt_target(wheel, remote_target, visibility):
-    fb_native.prebuilt_python_library(
-        name=wheel,
-        visibility=get_visibility(visibility, wheel),
-        binary_src=remote_target,
-    )
-    return ":" + wheel
 
 
 def _wheel_override_version_check(name, platform_versions):
@@ -123,27 +98,6 @@ def _error_rules(name, msg, visibility=None):
         exported_headers=[":{}-gen".format(name)],
         visibility=['PUBLIC'],
     )
-
-
-def _is_compiled(url):
-    """
-    Returns True if wheel with provided url is precompiled.
-
-    The logic in this method is a less efficient version of
-    -cp[0-9]{2}- regex matching.
-    """
-    prefix = "-cp"
-    start = 0
-    for _ in range(len(url)):
-        start = url.find(prefix, start)
-        if start == -1 or start + 6 >= len(url):
-            break
-        if url[start + len(prefix)].isdigit() and \
-                url[start + len(prefix) + 1].isdigit() and \
-                url[start + len(prefix) + 2] == "-":
-            return True
-        start += len(prefix)
-    return False
 
 
 class PyWheelDefault(base.Converter):
@@ -211,88 +165,6 @@ class PyWheel(base.Converter):
     def get_fbconfig_rule_type(self):
         return 'python_wheel'
 
-    def convert_rule(
-        self,
-        base_path,
-        version,
-        platform_urls,  # Dict[str, str]   # platform -> url
-        deps=(),
-        external_deps=(),
-        tests=(),
-        visibility=None,
-    ):
-        # We don't need duplicate targets if we have multiple usage of URLs
-        urls = set(platform_urls.values())
-        wheel_targets = {}  # Dict[str, str]      # url -> prebuilt_target_name
-
-        compiled = False
-        # Setup all the remote_file and prebuilt_python_library targets
-        # urls have #sha1=<sha1> at the end.
-        for url in urls:
-            if url is None:
-                continue
-            if _is_compiled(url):
-                compiled = True
-            orig_url, _, sha1 = url.rpartition('#sha1=')
-            assert sha1, "There is no #sha1= tag on the end of URL: " + url
-            # Opensource usage of this may have #md5 tags from pypi
-            wheel = get_url_basename(orig_url)
-            target_name = remote_wheel(url, wheel, sha1, visibility)
-            target_name = prebuilt_target(wheel, target_name, visibility)
-            wheel_targets[url] = target_name
-
-        attrs = {}
-
-        # Create the ability to override the platform that wheels use
-        wheel_platform = read_config("python", "wheel_platform_override")
-
-        # Use platform_deps to rely on the correct wheel target for
-        # each platform
-        attrs['platform_deps'] = [
-            ('{}$'.format(platform_utils.escape(py_platform)), None if url is None else [wheel_targets[url]])
-            for py_platform, url in sorted(platform_urls.items())
-            # Some platforms just do not have wheels available. In this case, we remove
-            # that platform from platform deps. You just won't get a whl on those
-            # platforms. HOWEVER: Due to how platforms work in buck, if there's a
-            # wheel_platform, we want to keep this platform. We keep it because a user
-            # might still get something like 'gcc5-blah' as the buck native platform
-            # even when we've overwritten all urls with say a mac specific url.
-            # It sucks, and when select() and platform support is in buck and handled
-            # properly by all rules, this will be wholly re-evaluated.
-            if url or wheel_platform
-        ]
-
-        if wheel_platform:
-            attrs['platform_deps'] = _override_wheels(attrs['platform_deps'], wheel_platform)
-
-        # This is to work around how buck instantiates toolchains. Without this,
-        # we don't always end up properly instantiating the c++ toolchains if
-        # the compiler is a python script. T34675852
-        cpp_genrule_name = version + "-genrule-hack"
-        native.cxx_genrule(name = cpp_genrule_name, out="dummy", cmd="echo '' > $OUT")
-        deps = (deps or []) + [":" + cpp_genrule_name]
-
-        if external_deps:
-            if compiled:
-                attrs['exclude_deps_from_merged_linking'] = True
-            attrs['platform_deps'].extend(
-                src_and_dep_helpers.format_platform_deps(
-                    [src_and_dep_helpers.normalize_external_dep(d, lang_suffix='-py')
-                     for d in external_deps]))
-
-        if tests:
-            attrs['tests'] = tests
-
-        # TODO: Figure out how to handle typing info from wheels
-        if get_typing_config_target():
-            gen_typing_config(version, visibility=visibility)
-        fb_native.python_library(
-            name=version,
-            deps=deps,
-            visibility=get_visibility(visibility, version),
-            **attrs
-        )
-
     def get_allowed_args(self):
         return {
             'version',
@@ -306,30 +178,11 @@ class PyWheel(base.Converter):
         """
         Entry point for converting python_wheel rules
         """
-        # in python3 this method becomes just.
-        # yield from self.convert_rule(base_path, name, **kwargs)
-        self.convert_rule(base_path, version, platform_urls, visibility=visibility, **kwargs)
+        python_wheel(
+            version=version,
+            platform_urls=platform_urls,
+            visibility=visibility,
+            **kwargs
+        )
 
         return []
-
-
-def _override_wheels(deps, wheel_platform):
-    # For all deps, override the current wheel file with the one corresponding
-    # to the specified wheel platform.
-
-    # We're doing this because platforms in the list of deps are also re.escaped.
-    wheel_platform = platform_utils.escape(wheel_platform)
-
-    override_urls = None
-    for platform, urls in deps:
-        if wheel_platform in platform:
-            override_urls = urls
-
-    if not override_urls:
-        return deps
-
-    new_deps = []
-    for platform, _ in deps:
-        new_deps.append((platform, override_urls))
-
-    return new_deps
