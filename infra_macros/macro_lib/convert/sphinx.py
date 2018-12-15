c879cb6eafda9369443b777273cb33ffe727e31f
Taras Tsugrii <ttsugrii@fb.com>
2018-12-15 12:53:54 -0800

    Factor out common sphinx pieces into sphinx_common.bzl.
    Reviewed By: andrewjcg
    
    Differential Revision: D13476289
    
    fbshipit-source-id: 02912a6535ea626491e2f2b9ebf3f282e9c198b3

--- infra_macros/macro_lib/convert/sphinx.py
+++ infra_macros/macro_lib/convert/sphinx.py
@@ -143,6 +143,7 @@ def import_macro_lib(path):
 base = import_macro_lib("convert/base")
 
 load("@fbcode_macros//build_defs/lib:target_utils.bzl", "target_utils")
+load("@fbcode_macros//build_defs/lib:sphinx_common.bzl", "sphinx_common")
 load("@fbsource//tools/build_defs:fb_native_wrapper.bzl", "fb_native")
 load("@fbcode_macros//build_defs:python_binary.bzl", "python_binary")
 
@@ -170,67 +171,6 @@ class _SphinxConverter(base.Converter):
     def get_buck_rule_type(self):
         return "genrule"
 
-    def _gen_genrule_srcs_rules(self, base_path, name, genrule_srcs):
-        """
-        A simple genrule wrapper for running some target which generates rst
-        """
-        if not genrule_srcs:
-            return []
-
-        rules = []
-
-        for target, outdir in genrule_srcs.items():
-            rule = target_utils.parse_target(target, default_base_path=base_path)
-            if "/" in outdir:
-                root, rest = outdir.split("/", 1)
-            else:
-                root = outdir
-                rest = "."
-            rule_name = name + "-genrule_srcs-" + rule.name
-            fb_native.genrule(
-                name=rule_name,
-                out=root,
-                bash=" ".join(
-                    (
-                        "mkdir -p $OUT/{rest} &&",
-                        "PYTHONWARNINGS=i $(exe {target})",
-                        "$OUT/{rest}",
-                    )
-                ).format(target=target, rest=rest),
-            )
-            rules.append(rule_name)
-
-        return rules
-
-    def _gen_apidoc_rules(self, base_path, name, fbsphinx_buck_target, apidoc_modules):
-        """
-        A simple genrule wrapper for running sphinx-apidoc
-        """
-        if not apidoc_modules:
-            return []
-
-        rules = []
-
-        for module, outdir in apidoc_modules.items():
-            command = " ".join(
-                (
-                    "mkdir -p $OUT &&",
-                    "PYTHONWARNINGS=i $(exe :{fbsphinx_buck_target})",
-                    "buck apidoc",
-                    module,
-                    "$OUT",
-                )
-            ).format(fbsphinx_buck_target=fbsphinx_buck_target)
-            rule_name = name + "-apidoc-" + module
-            fb_native.genrule(
-                name=rule_name,
-                out=outdir,
-                bash=command,
-            )
-            rules.append(rule_name)
-
-        return rules
-
     def convert(
         self,
         base_path,
@@ -247,67 +187,21 @@ class _SphinxConverter(base.Converter):
         """
         Entry point for converting sphinx rules
         """
-        python_deps = (
-            tuple(python_library_deps)
-            + tuple((_dep + "-library" for _dep in tuple(python_binary_deps)))
-            + (FBSPHINX_WRAPPER,)
-        )
-        fbsphinx_buck_target = "%s-fbsphinx-buck" % name
-        python_binary(
-            name=fbsphinx_buck_target,
-            par_style="xar",
-            py_version=">=3.6",
-            main_module="fbsphinx.bin.fbsphinx_buck",
-            deps=python_deps,
-        )
 
-        additional_doc_rules = []
-
-        additional_doc_rules.extend(
-            self._gen_apidoc_rules(
-                base_path, name, fbsphinx_buck_target, apidoc_modules
-            )
-        )
-
-        additional_doc_rules.extend(
-            self._gen_genrule_srcs_rules(base_path, name, genrule_srcs)
-        )
-
-        command = " ".join(
-            (
-                "echo {BUCK_NONCE} >/dev/null &&",
-                "PYTHONWARNINGS=i $(exe :{fbsphinx_buck_target})",
-                "buck run",
-                "--target {target}",
-                "--builder {builder}",
-                "--sphinxconfig $(location {SPHINXCONFIG_TGT})",
-                "--config '{config}'",
-                "--generated-sources '{generated_sources}'",
-                ".",  # source dir
-                "$OUT",
-            )
-        ).format(
-            BUCK_NONCE=read_config("sphinx", "buck_nonce", ""),
-            fbsphinx_buck_target=fbsphinx_buck_target,
-            target="//{}:{}".format(base_path, name),
-            builder=self.get_builder(),
-            SPHINXCONFIG_TGT=SPHINXCONFIG_TGT,
-            config=struct(config=(config or {})).to_json(),
-            generated_sources="[" + ",".join([
-                "\"$(location :{})\"".format(rule)
-                for rule in additional_doc_rules
-            ]) + "]",
-        )
-
-        # fb_native rule adds extra labels that genrule fails to swallow
-        native.genrule(
+        sphinx_common.sphinx_rule(
+            base_path=base_path,
             name=name,
-            type=self.get_fbconfig_rule_type(),
-            out="builder=%s" % self.get_builder(),
-            bash=command,
-            srcs=srcs,
+            rule_type=self.get_fbconfig_rule_type(),
+            builder=self.get_builder(),
             labels=self.get_labels(name, **kwargs),
+            apidoc_modules=apidoc_modules,
+            config=config,
+            genrule_srcs=genrule_srcs,
+            python_binary_deps=python_binary_deps,
+            python_library_deps=python_library_deps,
+            srcs=srcs
         )
+
         return []
 
 
