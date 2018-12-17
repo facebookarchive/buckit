8c49d17d9ce69e47b6503777375ae2f6bedc8ef1
Taras Tsugrii <ttsugrii@fb.com>
2018-12-17 15:11:21 -0800

    Move unrelated tests to appropriate package.
    Summary:
    These tests do not test anything related to macro_lib and
    instead test a validator package in tupperware.
    
    Reviewed By: WillerZ
    
    Differential Revision: D13478440
    
    fbshipit-source-id: ceafdf5e5e4e6bbf0c1f26abd10bc83452e6f221

--- infra_macros/macro_lib/convert/tests/thriftdoc/leaf1.thrift
+++
@@ -1,6 +0,0 @@
-namespace py leaf1
-
-struct Leaf1 {
-  /** @must _ >= 1 */
-  1: i32 leaf = 1
-}
--- infra_macros/macro_lib/convert/tests/thriftdoc/leaf2.thrift
+++
@@ -1,8 +0,0 @@
-namespace py leaf2
-
-/**
- * This is included in Parent.
- */
-struct Leaf2 {
-  1: i32 leaf = 2
-}
