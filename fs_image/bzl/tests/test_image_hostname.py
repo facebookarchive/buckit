e64976eb4fd4fdef9fd85e30bc9d4958982b8840
Lindsay Salisbury <lsalis@fb.com>
2019-12-04 18:14:54 -0800

    Re-sync with internal repository

--- fs_image/bzl/tests/test_image_hostname.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import socket
-import unittest
-
-
-class ImagePythonUnittestTest(unittest.TestCase):
-    def test_container(self):
-        # Ensure the hostname configuration was propagated inside the container
-        self.assertEqual("test-hostname.com", socket.gethostname())
--- fs_image/compiler/tests/print_ok.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-'Prints the unicode string "ok" via the `print` function to `stdout`, on 1 line'
-print('ok')
